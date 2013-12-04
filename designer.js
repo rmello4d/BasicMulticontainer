cbd889d15dd80868716b28bf52d08229cf3d27cd
Ricardo <ricardo.mello@4d.com>
2013-12-04 18:59:40 +0100

    adding class headers and license

--- designer.js
+++ designer.js
@@ -1,37 +1,6 @@
-(function() {
-    var BasicMulticontainer = Widget.BasicMulticontainer.inherit(WAF.require('waf-behavior/studio'));
+;(function(BasicMulticontainer) {
 
-    /*Display name of the widget*/
-	BasicMulticontainer.setDescription('BasicMulticontainer');
 
-	// adding attributes
-    BasicMulticontainer.addAttributes([{
-    	'name':'data-width',
-    	'description':'Width',
-    	'defaultValue': '250px'
-    },{
-    	'name':'data-height',
-    	'description':'Height',
-    	'defaultValue': '250px'
-    }]);  
-      
-    // Defining actions when user tries to resize the widget   
-    BasicMulticontainer.on('Resize', function(event) {
-    	this.studioCss('height', event.size.height);
-    	this.studioCss('width', event.size.width); 
-    	this.studioVal('data-width', event.size.width + 'px');
-    	this.studioVal('data-height', event.size.height + 'px');	 	
-	 	this.studioPanelRefresh();
-    });
-    
-
-    // Defining actions when widget is displayed/updated in the Studio
-	BasicMulticontainer.on('Display', function (attributes) {
-		//display width and height
-	 	this.studioCss('width', attributes['data-width']); 	
-	 	this.studioCss('height', attributes['data-height']); 		  
-	 });
-
-})();
+});
 
 // For more information, refer to http://doc.wakanda.org/Wakanda0.DevBranch/help/Title/en/page3870.html
--- widget.js
+++ widget.js
@@ -1,34 +1,23 @@
-(function() {
-	
-var widget = WAF.require('waf-core/widget');
+WAF.define('BasicMulticontainer', function() {
+    var Widget = WAF.require('waf-core/widget');
+    var BasicMulticontainer = Widget.create('BasicMulticontainer');
 
-//creating the widget 
-var BasicMulticontainer = widget.create('BasicMulticontainer');
+    BasicMulticontainer.inherit(WAF.require('waf-behavior/layout/multicontainer')); 
 
-//inheriting from multicontainer behavior
-BasicMulticontainer.inherit(WAF.require('waf-behavior/layout/multicontainer')); 
+    BasicMulticontainer.defaultContainer(Widget.BasicContainer);
 
-//defining actions when init	
-BasicMulticontainer.prototype.init = function() {
-	
-	//Adding 2 pages (and making sure that we do not create more pages unecessarily, so we count them first)
-	for(var i = this.countPages(); i < 2 ; i++) {
-		this.addPage({});
-	}
-	 
-	//setting the class for the last page 
-	this.lastPage().addClass('waf-active-state');
-	
-	//setting current page 
-	this.currentPageIndex(0);
-		        
-	//when a click happens in the widget, we switch from one Page to the other	        
-	$(this.getNode()).on('click', function() {
-			this.currentPageIndex(this.currentPageIndex() ? 0 : 1);
-	}.bind(this))	
-  	        
-}
+    BasicMulticontainer.prototype.init = function() {
+        //Adding 2 pages (and making sure that we do not create more pages unecessarily, so we count them first)
+        for(var i = this.countPages(); i < 2 ; i++) {
+            this.addPage({});
+        }
+        this.currentPageIndex(0);
 
+        $(this.getNode()).on('click', function() {
+            this.currentPageIndex(this.currentPageIndex() ? 0 : 1);
+        }.bind(this));
+    };
+    
+    return BasicMulticontainer;
 
-})();
-
+});
